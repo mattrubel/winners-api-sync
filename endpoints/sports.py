173276fca20c6228f90a385dc7e6c099a0536eda
Matt Rubel <mrubel92@gmail.com>
2023-02-04 21:55:28 -0500

    clean up code

--- endpoints/sports.py
+++ endpoints/sports.py
@@ -3,6 +3,8 @@ This module contains the object that interfaces with the sports endpoint
 """
 import datetime
 
+import pytz
+
 import util.utils as utils
 from interface.endpoint import Endpoint
 
@@ -11,14 +13,21 @@ class Sports(Endpoint):
     """
     This class will contain the functionality to call the sports endpoint
     """
-    def __init__(self, api_key: str, base_url: str, s3_bucket: str, aws_access_key: str, aws_secret_access_key: str):
+    def __init__(
+            self,
+            api_key: str,
+            base_url: str,
+            s3_bucket: str,
+            aws_access_key: str,
+            aws_secret_access_key: str
+    ):
         super().__init__()
         self.api_key = api_key
         self.base_url = base_url
         self.s3_bucket = s3_bucket
         self.call_type = "sports"
-        self.datetime_string = datetime.datetime.now(tz=pytz.UTC).strftime("%Y-%m-%d-%H-%M")
-        # self.s3_key = utils.get_s3_key("sports")
+        self.datetime_string = datetime.datetime.now(tz=pytz.UTC)\
+            .strftime("%Y-%m-%d-%H-%M")
         self.aws_access_key = aws_access_key
         self.aws_secret_access_key = aws_secret_access_key
 
@@ -28,7 +37,12 @@ class Sports(Endpoint):
         :param kwargs: not used.
         :return: string of payload
         """
-        return utils.call_get_endpoint(self.base_url, "", "sports", self.api_key)
+        return utils.call_get_endpoint(
+            self.base_url,
+            "",
+            "sports",
+            self.api_key
+        )
 
     def export_to_s3(self, payload: str):
         """
@@ -38,4 +52,10 @@ class Sports(Endpoint):
         """
         s3_key = utils.get_s3_key(self.call_type, self.datetime_string)
         byte_stream = bytearray(payload, "UTF-8")
-        utils.export_to_s3(byte_stream, self.s3_bucket, s3_key, self.aws_access_key, self.aws_secret_access_key)
+        utils.export_to_s3(
+            byte_stream,
+            self.s3_bucket,
+            s3_key,
+            self.aws_access_key,
+            self.aws_secret_access_key
+        )
--- test/tests.py
+++ test/tests.py
@@ -4,16 +4,21 @@ import unittest
 
 import util.utils as utils
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '')))
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), ''))
+)
 
 
 class TestUtils(unittest.TestCase):
     def test_build_url_with_api_key(self):
-        self.assertEqual(utils._build_url_with_api_key("baseurl", "parameterstring", "apikey"),
-                         "baseurl?apiKey=apikeyparameterstring")
+        self.assertEqual(
+            utils._build_url_with_api_key(
+                "baseurl",
+                "parameterstring",
+                "apikey"),
+            "baseurl?apiKey=apikeyparameterstring")
 
     def test_get_s3_key(self):
         self.assertEqual(utils.get_s3_key("sports", "2023-02-04-19-48"),
                          "raw-data/sports/2023/02/04/1948/sports.json")
-
-# Testing section in here. https://docs.python-guide.org/writing/structure/
