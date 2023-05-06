8a92896a93740f70b3fed2a7a0c27c112d4faf35
Matt Rubel <mrubel92@gmail.com>
2023-05-06 16:08:01 -0400

    create sport table

--- terraform/aws-resource-configs/dynamodb.tf
+++ terraform/aws-resource-configs/dynamodb.tf
@@ -25,3 +25,28 @@ resource "aws_dynamodb_table" "winners_api_sync_endpoint_logging_table" {
     type = "S"
   }
 }
+
+
+resource "aws_dynamodb_table" "winners_sports_table" {
+  name         = "winners-sports"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "sport_key"
+
+  global_secondary_index {
+    hash_key        = "collect"
+    range_key       = "sport_key"
+    name            = "collect_sport_key_idx"
+    projection_type = "ALL"
+  }
+
+  attribute {
+    name = "sport_key"
+    type = "S"
+  }
+
+  attribute {
+    name = "collect"
+    type = "B"
+  }
+
+}
